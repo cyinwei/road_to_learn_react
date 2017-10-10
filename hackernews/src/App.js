cc2b9f623ec8116ea391dace3a7368850c99dccc
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2017-10-10 10:52:13 -0400

    IntroReact: list.map() with React components.  Finished the chapter.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
+const pst = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        {pst.map(site =>
+          <div key={site.objectID}>
+            <h3><a href={site.url}>{site.title}</a></h3>
+              <p>
+                Author: {site.author}
+              </p>
+              <p>
+                {site.num_comments} comments
+              </p>
+              <p>
+                {site.points}
+              </p>
+          </div>
+         )}       
       </div>
-    );
+    )
   }
 }
 
--- hackernews/src/index.js
+++ hackernews/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+
+registerServiceWorker()
+
+// enables hot module reloading for development
+if (module.hot) {
+  module.hot.accept()
+}
