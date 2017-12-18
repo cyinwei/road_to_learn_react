01f7f3510521e77676c51d1312ea8f4f215a6993
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2017-12-17 22:51:10 -0500

    Done with conditional rendering in Getting Real with an API.

--- hackernews/src/App.jsx
+++ hackernews/src/App.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, 
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+const DEFAULT_QUERY = 'redux'
+
+const BASE_PATH = 'https://hn.algolia.com/api/v1'
+const PATH_SEARCH = '/search'
+const PARAM_SEARCH = 'query='
+
+const url = `${BASE_PATH}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
+const generateUrl = (searchTerm) =>
+  `${BASE_PATH}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`
+
+console.log(url)
 
 const isSearched = (searchTerm) =>
   (site) => {
@@ -38,7 +31,7 @@ const Search = ({value, onChange}) =>
   </form>
 
 const Table = ({list, pattern, onDismiss}) =>
-  <div classname="table">
+  <div className="table">
     {list.filter(isSearched(pattern)).map(site =>
       <div key={site.objectID} className="table-row">
         <h3 className="table-text" style={{width: '40%'}}>
@@ -50,8 +43,8 @@ const Table = ({list, pattern, onDismiss}) =>
         <Button
           onClick={() => onDismiss(site.objectID)}
           className="button-inline table-text"
-          >
-            dismiss
+        >
+          dismiss
         </Button>
       </div>
      )}  
@@ -71,13 +64,28 @@ class App extends Component {
     super(props)
 
     this.state = {
-      list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     }
 
     // binding this
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
     this.onSearchChange = this.onSearchChange.bind(this)
     this.onDismiss = this.onDismiss.bind(this)
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result})
+  }
+
+  fetchSearchTopStories(searchTerm) {
+    fetch(generateUrl(searchTerm), {
+      mode: 'cors'
+    })
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e)
   }
 
   onSearchChange(event) {
@@ -86,13 +94,24 @@ class App extends Component {
   }
 
   onDismiss(id) {
-    let updatedList = this.state.list.filter(site => 
-      id !== site.objectID)
+    let updatedHits = this.state.result.hits.filter(post => 
+      id !== post.objectID)
     
-    this.setState({list: updatedList})
+    console.log({...this.state.result})
+
+    this.setState({
+      result: {...this.state.result, hits: updatedHits}
+    })
+  }
+
+  componentDidMount() {
+    const {searchTerm} = this.state
+    this.fetchSearchTopStories(searchTerm)
   }
 
   render() {
+    const { searchTerm, result } = this.state
+    console.log(result)
     return <div className="page">
       <div className="interactions">
         <Search 
@@ -102,12 +121,15 @@ class App extends Component {
           Search
         </Search>
       </div>
-     
-      <Table
-        list={this.state.list}
-        pattern={this.state.searchTerm}
-        onDismiss={this.onDismiss}
-      />
+      { result ?
+        <Table
+          list={this.state.result.hits}
+          pattern={this.state.searchTerm}
+          onDismiss={this.onDismiss}
+        />
+        :
+        <p style={{textAlign: 'center'}}>Loading...</p>
+      }
     </div>
   }
 }
